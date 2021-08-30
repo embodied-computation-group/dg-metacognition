b9158968d4624f61a0e364bf7761d91f3bcb4a65
AstridLund <Astrid.lund@kcl.ac.uk>
2021-08-30 08:35:18 +0100

    download Hmeta-d master

--- r/fit_hMetaCorr.R
+++ r/fit_hMetaCorr.R
@@ -2,15 +2,15 @@ fit_HmetaCorr <- function (subjects, modalities, s, m) {
 
   library(here)
   library(metaSDT)
+  library(HMeta-d-master)
   
   source(here("r", "trials2counts.R"))
   source(here("r", "Bayes_metad_hierarchical.R"))
-  
 
 tryCatch(
   expr = {
     # subset data for subject and modality 
-    subdata <- filter(metadata, subject == subjects[s] & modality == modalities[m])  
+    subdata <- filter(metadata_exc, subject == subjects[s] & modality == modalities[m])  
     
     #transform trial data to counts
     newlist <- trials2counts(subdata$signal, subdata$response, subdata$confidence, 7, padAmount = 1,padCells=1)
