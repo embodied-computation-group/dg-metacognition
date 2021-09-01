a2b5490a54c266654a4939f6cdf54dd0d3613fe4
AstridLund <Astrid.lund@kcl.ac.uk>
2021-09-01 14:01:37 +0100

    Plots

--- r/fit_hMetaCorr.R
+++ r/fit_hMetaCorr.R
@@ -1,28 +1,56 @@
-fit_HmetaCorr <- function (subjects, modalities, s, m) {
-
+fit_HmetaCorr  <- function (subjects, modalities, s, m) {
+  
   library(here)
-  library(metaSDT)
-  library(HMeta-d-master)
   
   source(here("r", "trials2counts.R"))
+  source(here("r", "metad_indiv.R"))
   source(here("r", "Bayes_metad_hierarchical.R"))
-
-tryCatch(
-  expr = {
-    # subset data for subject and modality 
-    subdata <- filter(metadata_exc, subject == subjects[s] & modality == modalities[m])  
+  
+  tryCatch(
+    expr = {
+      # subset data for subject and modality 
+      subdata <- filter(metadata, subject == subjects[s] & modality == modalities[m])  
+      
+      #transform trial data to counts
+      newlist <- trials2counts(subdata$signal, subdata$response, subdata$confidence, 7, padAmount = 1,padCells=1)
+      nR_S1 <- unlist(newlist[1], recursive = TRUE, use.names = TRUE)
+      nR_S2 <- unlist(newlist[2], recursive = TRUE, use.names = TRUE)
+      
+      #fit model
+      fit <- metad_groupcorr(nR_S1, nR_S2)
+      #glimpse(subdata)
+      #Sys.sleep(1)
+      
+      Results <- coda.samples(model = fit$model, variable.names = fit$variable.names,
+                              n.iter=fit$n.iter, thin=fit$thin)
+        
+        "subject" = subjects[s],"modality"=modalities[m],
+                            "Corr" = fit$model, "mda" = fit$meta_da[1],
+                            "mratio" = fit$M_ratio[1], "c" = fit$criterion[1])  
+      
+      output <- coda.samples( 
+        model          = cor_model,
+        variable.names = c("mu_logMratio", "sigma_logMratio", "rho", "Mratio", "mu_c2"),
+        n.iter         = 10000,
+        thin           = 1 )
+      
+      
+      return(Results)  
+      
+    },
+    error = function(e) {
+      # message("* caught an error on subject ", subjects[s])
+      # print(e)
+      Results <- data.frame("subject" = subjects[s],
+                            "modality"=modalities[m],"da" = NaN,
+                            "mda" = NaN, "mratio" = NaN, "c" = NaN) 
+      
+      return(Results)  
+    }
     
-    #transform trial data to counts
-    newlist <- trials2counts(subdata$signal, subdata$response, subdata$confidence, 7, padAmount = 1,padCells=1)
-    nR_S1 <- unlist(newlist[1], recursive = TRUE, use.names = TRUE)
-    nR_S2 <- unlist(newlist[2], recursive = TRUE, use.names = TRUE)
     
-    fit <- metad_groupcorr(nR_s1, nR_S2)
-    
-    Results <- data.frame("subject" = subjects[s],"modality"=modalities[m],
-                          "corr"=fit$rho, "Mratio"=fit$Mratio)
-    return(Results)  
-  }
-)
+  )
+  
+  
   
 }
