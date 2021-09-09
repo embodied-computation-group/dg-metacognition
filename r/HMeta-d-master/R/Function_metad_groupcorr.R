2abc6f0f77dff55ee2ce5ea577af5ad18a1d30eb
AstridLund <Astrid.lund@kcl.ac.uk>
2021-09-09 09:53:14 +0100

    ...

--- r/HMeta-d-master/R/Function_metad_groupcorr.R
+++ r/HMeta-d-master/R/Function_metad_groupcorr.R
@@ -38,7 +38,7 @@ library(ggpubr)
 library(ggmcmc)
 
 library(here)
-source(here("txt", "Bayes_metad_group_corr4_R.txt"))
+source(here("r", "Bayes_metad_group_corr4_R.txt"))
 
 metad_groupcorr <- function (nR_S1, nR_S2) {
   
