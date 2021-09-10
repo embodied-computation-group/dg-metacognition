36898ca8745d2f590c7cbf05da90ffd2d43b578e
AstridLund <Astrid.lund@kcl.ac.uk>
2021-09-10 17:41:42 +0100

    scatter plots

---
+++ r/scatterplot.R
@@ -0,0 +1,8 @@
+scatter <- function (data, x,y) {
+  
+  ggplot(data=data, aes(x=x, y=y)) + theme_bw() +
+    geom_point() +
+    theme(axis.text = element_text(size = 14),
+          axis.title = element_text(size = 16)) + geom_smooth(method="lm", se=FALSE, size=1)
+  
+}
