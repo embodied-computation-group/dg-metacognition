de55f435af0ebf072cd5edbac4914a7185580693
micahgallen <micah@cfin.au.dk>
2021-09-10 14:53:54 +0200

    Added staircase plots

--- r/make_indivi_plots.R
+++ r/make_indivi_plots.R
@@ -3,16 +3,39 @@ memory <- metadata %>%filter(modality == c('memory'))
 knowledge <- metadata %>%filter(modality == c('trivia')) 
 percept <- metadata %>%filter(modality == c('vision')) 
 
-#Perception: 
-Pplot_list = list() 
+subjects <- unique(percept$subject)
 
-for(t in 1:length(percept$subject)) { 
-  Pplots <- (ggplot(percept) +
-               geom_step(aes(x=(trial), y=contrast)) +
-               xlab("trial") +
-               ylab("contrast") + theme_bw() + facet_wrap(percept$subject))
-  Pplot_list[[t]] = Pplots
-} #Create plots for each participant
+this_data <- percept %>% filter(subject == subjects[t])
+
+#Perception:
+for(t in seq(1,length(subjects), 12) ) { 
+  plots = list()
+  counter = 0
+  for(p in seq(t, t+11)) {
+    
+    counter = counter + 1;
+    tmp_data <- percept %>% filter(subject == subjects[p])
+    this_median = median(median(tmp_data$contrast))
+    thisPlot <- ggplot(tmp_data) +
+    geom_step(aes(x=(trial), y=contrast)) +
+    xlab("trial") +
+    ylim(-5, 30) +
+    xlim(0, 200)+ 
+    geom_hline(yintercept = this_median) +
+    ylab("contrast") + theme_bw() +
+    ggtitle(paste("subject", subjects[p], "visStair", sep = "_"))
+    plots[[counter]] <- thisPlot
+    
+    
+  } 
+  
+  filename=paste("staircase",subjects[t], "_", subjects[t+11],".png",sep="")
+  wrap_plots(plots)
+  ggsave(here("figs", filename))
+
+}
+
+#Create plots for each participant
 
 #save to pdf with 20 participants on each page: 
 pdf("staircaseplots.pdf")
