55685d56bcb7136a83e99f3478474fce939ce735
AstridLund <Astrid.lund@kcl.ac.uk>
2021-10-25 17:12:02 +0100

    Tabel 2

---
+++ r/function_apa.R
@@ -0,0 +1,38 @@
+apa <- function(x, title = " ") {
+  gt(x) %>%
+    tab_options(
+      table.border.top.color = "white",
+      heading.title.font.size = px(16),
+      column_labels.border.top.width = 3,
+      column_labels.border.top.color = "black",
+      column_labels.border.bottom.width = 3,
+      column_labels.border.bottom.color = "black",
+      table_body.border.bottom.color = "black",
+      table.border.bottom.color = "white",
+      table.width = pct(100),
+      table.background.color = "white"
+    ) %>%
+    cols_align(align="center") %>%
+    tab_style(
+      style = list(
+        cell_borders(
+          sides = c("top", "bottom"),
+          color = "white",
+          weight = px(1)
+        ),
+        cell_text(
+          align="center"
+        ),
+        cell_fill(color = "white", alpha = NULL)
+      ),
+      locations = cells_body(
+        columns = everything(),
+        rows = everything()
+      )
+    ) %>%
+    #title setup
+    tab_header(
+      title = html("<i>", title, "</i>")
+    ) %>%
+    opt_align_table_header(align = "left")
+}
