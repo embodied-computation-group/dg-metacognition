74217a614fcb3beede3251b2c00069f4abaa1d9b
AstridLund <astrid.lund@kcl.ac.uk>
2023-12-14 15:04:22 +0000

    estimation of MLE and HMETA without repeat

---
+++ r/parMLEfitrep.R
@@ -0,0 +1,8 @@
+
+mle_fit_dataRep  <-  foreach (s = 1:length(subjects), .combine=rbind, .errorhandling = 'pass') %:%   
+  
+  foreach (m = 1:length(modalities), .combine = rbind)  %dopar% {
+    
+    fit_mlerep(subjects, modalities, s, m)
+    
+  }
