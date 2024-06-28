0c38aba4eb720a143d126b7c7d4c313a900bc536
AstridLund <astrid.lund@kcl.ac.uk>
2024-06-28 05:57:55 +0100

    MLE correlation added to mainscript + generate some figures.

--- r/Function_scatterplot.R
+++ r/Function_scatterplot.R
@@ -1,6 +1,6 @@
 scatter <- function (data, x,y) {
   ggplot(data=data, aes(x=x, y=y)) + theme_bw() +
-    geom_point() +
+    geom_point(shape = 21, fill = NA) +
     theme(axis.text = element_text(size = 14),
           axis.title = element_text(size = 16)) + geom_smooth(method="lm", se=FALSE, size=1) +
     guides(fill="none") + 
